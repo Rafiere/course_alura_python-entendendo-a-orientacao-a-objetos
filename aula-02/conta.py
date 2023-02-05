015c201f4392bd53132bad27aba9a76a95aba199
Rafiere <rafapgbr@gmail.com>
2023-02-04 22:27:00 -0300

    feat: add lesson 02 code

---
+++ aula-02/conta.py
@@ -0,0 +1,12 @@
+class Conta:
+
+    def __str__(self):
+        return f"Conta número: {self.numero}, titular: {self.titular}, saldo: {self.saldo}, limite: {self.limite}"
+
+    # A função "init" é uma função construtora.
+    # O "self" é a referência que sabe encontrar o objeto que está sendo construído nessa função.
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
---
+++ aula-02/teste.py
@@ -0,0 +1,5 @@
+from conta import Conta
+
+conta = Conta(123, "Nico", 55.5, 1000.0)
+
+print(str(conta))
