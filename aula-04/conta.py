94db46bf371bc5bb35c0af6537980240eac20936
Rafiere <rafapgbr@gmail.com>
2023-02-05 08:34:11 -0300

    feat: add lesson 04 files

--- aula-04/conta.py
+++ aula-04/conta.py
@@ -1,21 +1,25 @@
 class Conta:
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
     def __str__(self):
-        return f"Conta número: {self.numero}, titular: {self.titular}, saldo: {self.saldo}, limite: {self.limite}"
+        return f"Conta número: {self.__numero}, titular: {self.__titular}, saldo: {self.__saldo}, limite: {self.__limite}"
 
     # A função "init" é uma função construtora.
     # O "self" é a referência que sabe encontrar o objeto que está sendo construído nessa função.
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
